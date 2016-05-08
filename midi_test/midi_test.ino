747fa0cd5a89b7166f0de89803be482963f4a11a
gregoryhklein@gmail.com <gregoryhklein@gmail.com>
2016-05-07 23:09:13 -0400

    Using script from the internet -- adjusted motor speed and motor delay

--- midi_test/midi_test.ino
+++ midi_test/midi_test.ino
@@ -1,171 +1,533 @@
+int LaserState = LOW;                    // The variable that stores the state of the laser beam.
+int sensor = 8;                         // Change this value to calibrate your harp's sensor
+int delaylaser = 5;                     // If you increase this, the laser will be brighter, but the harp will be less fluid
+int delaymotor = 1;                     // This variable affects the speed, and fluidity of the harp.
 
-enum Note {
-  C, CSharp, D, DSharp, E, F, FSharp, G, GSharp, A, ASharp, B
-};
+int LaserPin =  7;                       // Tell the arduino that the laser is on pin 7
 
-enum Octave {
-  Third, Fourth
-};
+int motorPin1 = 8;                      //Use these names for the pin numbers.
+int motorPin2 = 9;
+int motorPin3 = 10;
+int motorPin4 = 11;
 
-int cMidiValue(Octave octave)
+int lookup[8] = {B01000, B01100, B00100, B00110, B00010, B00011, B00001, B01001};
+int motorSpeed = 950;
+
+int note9 = 0x61;
+int note8 = 0x63;
+int note7 = 0x64;
+int note6 = 0x66;
+int note5 = 0x68;
+int note4 = 0x70;
+int note3 = 0x71;
+int note2 = 0x40;
+int note1 = 0x47;
+
+int a, b, c, d, e, f, g, h, i = 0;       // Iniating the note status markers.
+
+void setup()
 {
-  switch (octave) {
-    case Third: return 36;
-    case Fourth: return 48;
-  }
+
+
+
+  pinMode(8, OUTPUT);                    // Setup for the motor.
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+
+
+  pinMode(LaserPin, OUTPUT);            // Setup for laser.
+
+
+  pinMode(13, OUTPUT);                 // Setup for status led.
+
+
+  Serial.begin(31250);                 //  Start a serial communication channel for MIDI
 }
 
-int cSharpMidiValue(Octave octave)
+void noteOn(int cmd, int pitch, int velocity)     // Function to play the notes
 {
-  switch (octave) {
-    case Third: return 37;
-    case Fourth: return 49;
-  }
-}
-
-int dMidiValue(Octave octave)
-{
-  switch (octave) {
-    case Third: return 38;
-    case Fourth: return 50;
-  }
-}
-
-int dSharpMidiValue(Octave octave)
-{
-  switch (octave) {
-    case Third: return 39;
-    case Fourth: return 51;
-  }
-}
-
-int eMidiValue(Octave octave)
-{
-  switch (octave) {
-    case Third: return 40;
-    case Fourth: return 52;
-  }
-}
-
-int fMidiValue(Octave octave)
-{
-  switch (octave) {
-    case Third: return 41;
-    case Fourth: return 53;
-  }
-}
-
-int fSharpMidiValue(Octave octave)
-{
-  switch (octave) {
-    case Third: return 42;
-    case Fourth: return 54;
-  }
-}
-
-int gMidiValue(Octave octave)
-{
-  switch (octave) {
-    case Third: return 43;
-    case Fourth: return 55;
-  }
-}
-
-int gSharpMidiValue(Octave octave)
-{
-  switch (octave) {
-    case Third: return 44;
-    case Fourth: return 56;
-  }
-}
-
-int aMidiValue(Octave octave)
-{
-  switch (octave) {
-    case Third: return 45;
-    case Fourth: return 57;
-  }
-}
-
-int aSharpMidiValue(Octave octave)
-{
-  switch (octave) {
-    case Third: return 46;
-    case Fourth: return 58;
-  }
-}
-
-int bMidiValue(Octave octave)
-{
-  switch (octave) {
-    case Third: return 47;
-    case Fourth: return 59;
-  }
-}
-
-int midiValue(Note note, Octave octave)
-{
-  switch (note) 
-  {
-    case C: return cMidiValue(octave);
-    case CSharp: return cSharpMidiValue(octave);
-    case D: return dMidiValue(octave);
-    case DSharp: return dSharpMidiValue(octave);
-    case E: return eMidiValue(octave);
-    case F: return fMidiValue(octave);
-    case FSharp: return fSharpMidiValue(octave);
-    case G: return gMidiValue(octave);
-    case GSharp: return gSharpMidiValue(octave);
-    case A: return aMidiValue(octave);
-    case ASharp: return aSharpMidiValue(octave);
-    case B: return bMidiValue(octave);
-    default: return cMidiValue(octave);
-  }
-}
-
-void setup() 
-{
-  Serial.begin(31250);
-}
-
-void playNote(Note note, Octave octave)
-{
-  Serial.write(0x90);
-
-  int value = midiValue(note, octave);
-  Serial.write(value);
-
-  int velocity = 0x7F;
+  Serial.write(cmd);
+  Serial.write(pitch);
   Serial.write(velocity);
 }
 
-void stopNote(Note note, Octave octave)
+void anticlockwise()
 {
-  Serial.write(0x90);
-
-  int value = midiValue(note, octave);
-  Serial.write(value);
-  Serial.write(0x00);
-}
-
-void playNote(Note note, Octave octave, float duration)
-{ 
-  playNote(note, octave);
-  delay(duration * 1000);
-
-  stopNote(note, octave);
-}
-
-void loop() 
-{
-  Note notes[] = {C, D, E, FSharp, GSharp, ASharp, C};
-  Octave octaves[] = {Third, Third, Third, Third, Third, Third, Fourth};
-
-  for (int i = 0; i < 7; i = i + 1) 
+  for(int i = 0; i < 8; i++)
   {
-    Note n = notes[i];
-    Octave o = octaves[i];
-    
-    playNote(n, o, 0.25);
+    setOutput(i);
+    delayMicroseconds(motorSpeed);
   }
+  for(int i = 0; i < 8; i++)
+  {
+    setOutput(i);
+    delayMicroseconds(motorSpeed);
+  }
+  for(int i = 0; i < 8; i++)
+  {
+    setOutput(i);
+    delayMicroseconds(motorSpeed);
+  }
+}
+
+void clockwise()
+{
+  for(int i = 7; i >= 0; i--)
+  {
+    setOutput(i);
+    delayMicroseconds(motorSpeed);
+  }
+  for(int i = 7; i >= 0; i--)
+  {
+    setOutput(i);
+    delayMicroseconds(motorSpeed);
+  }
+  for(int i = 7; i >= 0; i--)
+  {
+    setOutput(i);
+    delayMicroseconds(motorSpeed);
+  }
+}
+
+void setOutput(int out)
+{
+  digitalWrite(motorPin1, bitRead(lookup[out], 0));
+  digitalWrite(motorPin2, bitRead(lookup[out], 1));
+  digitalWrite(motorPin3, bitRead(lookup[out], 2));
+  digitalWrite(motorPin4, bitRead(lookup[out], 3));
+}
+
+void loop() {
+
+  digitalWrite(LaserPin, HIGH);               // Turn on the laser for the 1st beam.
+  delay(delaylaser);
+
+  if (true || (analogRead(0) > sensor ) && (a == 0) ) // If the sensor gets a signal, and the not is not playing:
+
+  {
+    digitalWrite(13, HIGH);       // Switch on status led.
+    noteOn(0x90, note1, 0x7F);    // Play note 1
+    a++;                          // Change the status variable to one.
+  }
+
+
+  else if(false && analogRead(0) < sensor )             // If the sensor does not get a signal:
+
+  {
+    digitalWrite(13, LOW);         // Switch off the status led.
+    noteOn(0x90, note1, 0x00);     // Stop playing note 1.
+    a = 0;                         // Change the status variable to zero.
+  }
+
+
+  digitalWrite(LaserPin, LOW);                // Turn off the Laser.
+
+        clockwise();
+  delay(delaymotor);                          // Small pause
+
+
+  digitalWrite(LaserPin, HIGH);              // Turn on the laser for the 2nd beam.
+  delay(delaylaser);
+
+  if((analogRead(0) > sensor ) && (b == 0) ) // If the sensor gets a signal, and the not is not playing:
+
+  {
+    digitalWrite(13, HIGH);              // Switch on status led.
+    noteOn(0x90, note2, 0x7F);           // Play note 2
+    b++;                                 // Change the status variable to one.
+  }
+
+
+  else if((analogRead(0) < sensor) )           // If the sensor does not get a signal:
+
+  {
+    digitalWrite(13, LOW);          // Switch off the status led.
+    noteOn(0x90, note2, 0x00);      // Stop playing note 2.
+    b = 0;                          // Change the status variable to zero.
+  }
+
+                  // Change the status variable to one.
+
+  digitalWrite(LaserPin, LOW);                    // Turn off the Laser.
+
+
+
+  clockwise();
+  delay(delaymotor);                          // Small pause
+
+
+  digitalWrite(LaserPin, HIGH);             // Turn on the laser for the 3rd beam.
+  delay(delaylaser);
+
+  if( (analogRead(0) > sensor ) && (c == 0) ) // If the sensor gets a signal, and the not is not playing:
+
+  {
+    digitalWrite(13, HIGH);       // Switch on status led.
+    noteOn(0x90, note3, 0x7F);    // Play note 3
+    c++;                          // Change the status variable to one.
+  }
+
+
+  else if(analogRead(0) < sensor )           // If the sensor does not get a signal:
+  {
+    digitalWrite(13, LOW);        // Switch off the status led.
+    noteOn(0x90, note3, 0x00);    // Stop playing note 2.
+    c = 0;                        // Change the status variable to zero.
+  }
+
+  digitalWrite(LaserPin, LOW);                 // Turn off the Laser.
+
+  clockwise();
+  delay(delaymotor);                            // Small pause
+
+  // Contiue in the same way for the rest of the code.
+
+  digitalWrite(LaserPin, HIGH);               //This is beam 4
+  delay(delaylaser);
+
+  if( (analogRead(0) > sensor ) && (d == 0) )
+  {
+    digitalWrite(13, HIGH);
+    noteOn(0x90, note4, 0x7F);
+    d++;
+  }
+
+
+  else if(analogRead(0) < sensor )
+  {
+    digitalWrite(13, LOW);
+    noteOn(0x90, note4, 0x00);
+    d = 0;
+  }
+
+  digitalWrite(LaserPin, LOW);
+
+
+
+  clockwise();
+  delay(delaymotor);
+
+
+  digitalWrite(LaserPin, HIGH);                  //This is beam 5
+  delay(delaylaser);
+
+  if( (analogRead(0) > sensor ) && (e == 0) )
+
+  {
+    digitalWrite(13, HIGH);
+    noteOn(0x90, note5, 0x7F);
+    e++;
+  }
+
+
+  else if(analogRead(0) < sensor )
+
+  {
+    digitalWrite(13, LOW);
+    noteOn(0x90, note5, 0x00);
+    e = 0;
+  }
+
+  digitalWrite(LaserPin, LOW);
+
+
+  clockwise();
+  delay(delaymotor);
+
+
+  digitalWrite(LaserPin, HIGH);                  //This is beam 6
+  delay(delaylaser);
+
+  if( (analogRead(0) > sensor ) && (f == 0) )
+
+  {
+    digitalWrite(13, HIGH);
+    noteOn(0x90, note6, 0x7F);
+    f++;
+  }
+
+
+  else if(analogRead(0) < sensor )
+
+  {
+    digitalWrite(13, LOW);
+    noteOn(0x90, note6, 0x00);
+    f = 0;
+  }
+
+  digitalWrite(LaserPin, LOW);
+
+  clockwise();
+  delay(delaymotor);
+
+
+  digitalWrite(LaserPin, HIGH);                  //This is beam 7
+  delay(delaylaser);
+
+  if( (analogRead(0) > sensor ) && (g == 0) )
+
+  {
+    digitalWrite(13, HIGH);
+    noteOn(0x90, note7, 0x7F);
+    g++;
+  }
+
+
+  else if(analogRead(0) < sensor )
+
+  {
+    digitalWrite(13, LOW);
+    noteOn(0x90, note7, 0x00);
+    g = 0;
+  }
+
+  digitalWrite(LaserPin, LOW);
+
+
+
+  clockwise();
+  delay(delaymotor);
+
+
+  digitalWrite(LaserPin, HIGH);                   //This is beam 8
+  delay(delaylaser);
+
+  if( (analogRead(0) > sensor ) && (h == 0) )
+
+  {
+    digitalWrite(13, HIGH);
+    noteOn(0x90, note8, 0x7F);
+    h++;
+  }
+
+
+  else if(analogRead(0) < sensor )
+
+  {
+    digitalWrite(13, LOW);
+    noteOn(0x90, note8, 0x00);
+    h = 0;
+  }
+
+  digitalWrite(LaserPin, LOW);
+
+
+
+  clockwise();
+  delay(delaymotor);
+
+
+  digitalWrite(LaserPin, HIGH);    //This is beam 9
+  delay(delaylaser);
+
+  if( (analogRead(0) > sensor ) && (i == 0) )
+  {
+    digitalWrite(13, HIGH);
+    noteOn(0x90, note9, 0x7F);
+    i++;
+  }
+
+  else if(analogRead(0) < sensor )
+
+  {
+    digitalWrite(13, LOW);
+    noteOn(0x90, note9, 0x00);
+    i = 0;
+  }
+
+
+
+  digitalWrite(LaserPin, LOW);
+
+
+  anticlockwise();
+  delay(delaymotor);
+
+  digitalWrite(LaserPin, HIGH);       //This is beam 8
+  delay(delaylaser);
+
+  if( (analogRead(0) > sensor ) && (h == 0) )
+
+  {
+    digitalWrite(13, HIGH);
+    noteOn(0x90, note8, 0x7F);
+    h++;
+  }
+
+
+  else if(analogRead(0) < sensor )
+
+  {
+    digitalWrite(13, LOW);
+    noteOn(0x90, note8, 0x00);
+    h = 0;
+  }
+
+  digitalWrite(LaserPin, LOW);
+
+
+
+  anticlockwise();
+  delay(delaymotor);
+
+
+  digitalWrite(LaserPin, HIGH);    //This is beam 7
+  delay(delaylaser);
+
+  if( (analogRead(0) > sensor ) && (g == 0) )
+
+  {
+    digitalWrite(13, HIGH);
+    noteOn(0x90, note7, 0x7F);
+    g++;
+  }
+
+
+  else if(analogRead(0) < sensor )
+  {
+    digitalWrite(13, LOW);
+    noteOn(0x90, note7, 0x00);
+    g = 0;
+  }
+
+  digitalWrite(LaserPin, LOW);
+
+
+  anticlockwise();
+  delay(delaymotor);
+
+
+  digitalWrite(LaserPin, HIGH);    //This is beam 6
+  delay(delaylaser);
+
+  if( (analogRead(0) > sensor ) && (f == 0) )
+
+  {
+
+    digitalWrite(13, HIGH);
+    noteOn(0x90, note6, 0x7F);
+    f++;
+  }
+
+
+  else if(analogRead(0) < sensor )
+
+  {
+    digitalWrite(13, LOW);
+    noteOn(0x90, note6, 0x00);
+    f = 0;
+  }
+
+  digitalWrite(LaserPin, LOW);
+
+
+  anticlockwise();
+  delay(delaymotor);
+
+
+  digitalWrite(LaserPin, HIGH);    //This is beam 5
+  delay(delaylaser);
+
+  if( (analogRead(0) > sensor ) && (e == 0) )
+
+  {
+    digitalWrite(13, HIGH);
+    noteOn(0x90, note5, 0x7F);
+    e++;
+  }
+
+
+  else if(analogRead(0) < sensor )
+
+  {
+    digitalWrite(13, LOW);
+    noteOn(0x90, note5, 0x00);
+    e = 0;
+  }
+
+  digitalWrite(LaserPin, LOW);
+
+  anticlockwise();
+  delay(delaymotor);
+
+
+  digitalWrite(LaserPin, HIGH);      //This is beam 4
+  delay(delaylaser);
+
+  if( (analogRead(0) > sensor ) && (d == 0) )
+
+  {
+    digitalWrite(13, HIGH);
+    noteOn(0x90, note4, 0x7F);
+    d++;
+  }
+
+  else if(analogRead(0) < sensor )
+
+  {
+    digitalWrite(13, LOW);
+    noteOn(0x90, note4, 0x00);
+    d = 0;
+  }
+
+  digitalWrite(LaserPin, LOW);
+
+  anticlockwise();
+  delay(delaymotor);
+
+
+  digitalWrite(LaserPin, HIGH);    //This is beam 3
+  delay(delaylaser);
+
+  if( (analogRead(0) > sensor ) && (c == 0) )
+  {
+    digitalWrite(13, HIGH);
+    noteOn(0x90, note3, 0x7F);
+    c++;
+  }
+
+
+  else if(analogRead(0) < sensor )
+
+  {
+    digitalWrite(13, LOW);
+    noteOn(0x90, note3, 0x00);
+    c = 0;
+  }
+
+  digitalWrite(LaserPin, LOW);
+
+  anticlockwise();
+  delay(delaymotor);
+
+  digitalWrite(LaserPin, HIGH);    //This is beam 2
+  delay(delaylaser);
+
+  if(true || (analogRead(0) > sensor ) && (b == 0) )
+
+  {
+    digitalWrite(13, HIGH);
+    noteOn(0x90, note2, 0x7F);
+    b++;
+  }
+
+
+  else if(analogRead(0) < sensor )
+
+  {
+    digitalWrite(13, LOW);
+    noteOn(0x90, note2, 0x00);
+    b = 0;
+  }
+
+  digitalWrite(LaserPin, LOW);
+
+
+  anticlockwise();
+  delay(delaymotor);
+
+
+// Back to top.
+
 }
