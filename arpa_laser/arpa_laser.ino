9453981b8d6f057358e52c210a4910c9fe90ad9f
gregoryhklein@gmail.com <gregoryhklein@gmail.com>
2016-05-12 02:39:40 -0400

    Made things a little more readable

--- arpa_laser/arpa_laser.ino
+++ arpa_laser/arpa_laser.ino
@@ -1,10 +1,9 @@
 #include <Stepper.h>
 
-// FOR DEBUGGING!
 bool DEBUG_MODE = true;
 
 /*----- Stepper-related Constants -----*/
-#define STEPS 48
+#define STEPS_FOR_FULL_ROTATION 48
 #define STEP_DISTANCE 1
 #define STEPPER_SPEED 200
 
@@ -14,12 +13,10 @@ bool DEBUG_MODE = true;
 
 /*----- Note-related Constants -----*/
 #define TOTAL_NOTES 8
+#define MIDI_PORT 31250
 
 /*----- Declare objects -----*/
-// The pin connections need to be 4 pins connected
-// to Motor Driver In1, In2, In3, In4  and then the pins entered
-// here in the sequence 1-3-2-4 for proper sequencing
-Stepper _stepper(STEPS, 8, 9, 10, 11);
+Stepper stepper(STEPS_FOR_FULL_ROTATION, 8, 9, 10, 11);
 
 /*---- Setup Methods ----*/
 void _setupLaserPin()
@@ -29,7 +26,7 @@ void _setupLaserPin()
 
 void _connectToMIDI()
 {
-  Serial.begin(31250);
+  Serial.begin(MIDI_PORT);
 }
 
 void _connectToConsoleForDebugging()
@@ -60,7 +57,7 @@ void printSensorValueToConsoleIfInDebug()
 void setup()
 {
   _setupLaserPin();
-  _stepper.setSpeed(STEPPER_SPEED); 
+  stepper.setSpeed(STEPPER_SPEED); 
   
   if (DEBUG_MODE) {
     _connectToConsoleForDebugging();
@@ -73,24 +70,24 @@ void setup()
 void loop()
 {
   printSensorValueToConsoleIfInDebug();
-  
-  // Turn Laser on
-  // Read Photoresistor Value
-  // Do note stuff
-  // Turn laser off
-  // Step
 
-   for (int noteIndex = 0; noteIndex < TOTAL_NOTES; ++noteIndex) {
-      turnLaserON();
-      _stepper.step(1);
-      turnLaserOFF();
-      delay(1); 
-   }
+  for (int noteIndex = 0; noteIndex < TOTAL_NOTES; ++noteIndex) 
+  {
+    turnLaserON();
+    // read photoresistor value and play or turn note off
+    turnLaserOFF();
+    
+    stepper.step(1);
+    delay(1); 
+  }
    
-   for (int noteIndex = TOTAL_NOTES; noteIndex > 0 ; --noteIndex) {
-      turnLaserON();
-      _stepper.step(-1);
-      turnLaserOFF();
-      delay(1);
-   }
-} /* --(end main loop )-- */
+  for (int noteIndex = TOTAL_NOTES; noteIndex > 0 ; --noteIndex) 
+  {
+    turnLaserON();
+    // read photoresistor value and play or turn note off
+    turnLaserOFF();
+    
+    stepper.step(-1);
+    delay(1);
+  }
+}
